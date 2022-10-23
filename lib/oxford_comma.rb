ff4d42e6f50356ad88f9b283026bc82dec5601c1
fliblip <wickynjy@gmail.com>
2022-10-23 19:56:20 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
-
+    return array.join unless array.length > 1
+    return "#{array[0]} and #{array[1]}" unless array.length > 2
+    return array.slice(0, array.length-1).join(", ") +", and #{array[-1]}"
 end
